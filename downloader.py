7d22c1bcdb9e16863b4efd40c742b6465e6fc80f
Freezman13 <58755601+Freezman13@users.noreply.github.com>
2020-02-19 22:38:51 -0600

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,15 +1,17 @@
 import os
 import urllib.request
 
+# stores current directory
 path = os.getcwd()
 print ("The current working directory is %s" % path)
 
-# define the name of the directory to be created
-path += "/test"
+# adds the names of the new directory to be created
+path += "/tiles/"
 
 # define the access rights
 access_rights = 0o755
 
+# creates the tiles directory
 try:
     os.mkdir(path, access_rights)
 except OSError:
@@ -17,6 +19,32 @@ except OSError:
 else:
     print ("Successfully created the directory %s" % path)
 
-for x in range(1):
+# number of main subfolders
+var_folders = 8
+# array to be used in loops
+ar_loop = [1,5,9,17,33,65,129]
+# arry to be used for number of tiles
+ar_tiles = [0,4,8,16,32,64,128]
+# itteration variable
+i = 0
+
+# creates directories 0-7 inside tiles
+for x in range(var_folders):
     print(x)
-    urllib.request.urlretrieve("https://www.shiftingsands.gg//img/Leaflet/Maps/Canyon/{}/0/0.png".format(x), "map.png")
+    try:
+        os.mkdir("{}/{}".format(path,x), access_rights)
+    except OSError:
+        print ("Creation of the directory %s failed" % x)
+    else:
+        print ("Successfully created the directory %s" % x)
+   
+
+for x in range(var_folders): # loops 8 times
+    for y in range(ar_loop[i]):
+        urllib.request.urlretrieve("https://www.shiftingsands.gg//img/Leaflet/Maps/Canyon/0/0/0.png" \
+        , "{}/{}/{}.png".format(path,x,y))
+    ++i
+    
+    
+    
+    
